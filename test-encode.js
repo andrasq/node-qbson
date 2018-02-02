b6ef35395529ba754bc199324eb21538b6e40255
Andras <andras@kinvey.com>
2018-02-01 23:57:51 -0500

    more encode tests

--- test-encode.js
+++ test-encode.js
@@ -11,9 +11,17 @@ var data = [
     [ new Buffer("AAAA"), "1100000005610004000000004141414100" ],
     [ Symbol("Symbol Name"), "180000000e61000c00000053796d626f6c204e616d650000" ],
     [ undefined, "0800000006610a00" ],
-    [ qbson.ObjectId('0102030405060708090a0b0c'),       "14 00 00 00 07 61 00 01 02 03 04 05 06 07 08 09 0a 0b 0c 00" ],
-    [ new bsonTypes.Timestamp(1, 2),                    "10 00 00 00 11 61 00 02 00 00 00 01 00 00 00 00" ],
+    [ qbson.ObjectId('0102030405060708090a0b0c'), "14 00 00 00 07 61 00 01 02 03 04 05 06 07 08 09 0a 0b 0c 00" ],
+    [ new qbson.Timestamp(1, 2), "10 00 00 00 11 61 00 02 00 00 00 01 00 00 00 00" ],
     // [ new bsonTypes.DbRef("collname", new bsonTypes.ObjectId("12345678abcd")), "1d0000000c6100636f6c6c6e616d650031323334353637386162636400" ],
+    [ new qbson.Long(0x10000000, 2), "10000000126100020000000000001000" ],
+    [ new qbson.MinKey(), "08000000ff610000" ],
+    [ new qbson.MaxKey(), "080000007f610000" ],
+
+    [ new String("abc"), "10000000 02 6100 04000000 61626300 00" ],
+    // NOTE: BSON.serialize encodes `new Number(1)` as the empty object {}
+    // TODO: [ new Number(1.5), "0c000000 10 6100 01000000 00" ]
+    // FIXME: [ new Number(1), "0c000000 10 6100 01000000 00" ]
 ];
 for (var i=0; i<data.length; i++) {
     // squeeze out spaces from the hex string, for easier cut-and-paste
@@ -32,31 +40,29 @@ for (var i=0; i<data.length; i++) {
 
     var decoded = BSON.deserialize(buf);
 
-    switch (decoded.a._bsontype) {
-    // BSON decodes binary data into an encapsulated object, not a buffer,
-    // but encodes a Buffer into subtype 0 binary (ie, encode is not reversible)
-    case 'Binary':
+    switch (data[i][0].constructor) {
+    case Buffer:
+        // { _bsontype: 'Binary', buffer: buf }
         assert.deepEqual(decoded.a.buffer, data[i][0]);
         break;
-
-    // BSON decodes Symbol into an internal object type whose typeof is not 'symbol'
-    case 'Symbol':
+    case Symbol:
         assert.deepEqual("Symbol(" + decoded.a + ")", data[i][0].toString());
         break;
-
-    case 'ObjectID':
+    case qbson.ObjectId:
         assert.equal(decoded.a, String(data[i][0]));
         break;
-
-    case 'Timestamp':
+    case qbson.Timestamp:
         // high 4 bytes are timestamp, low 4 bytes are sequence number
         assert.equal(decoded.a.high_, data[i][0].time);
         assert.equal(decoded.a.low_, data[i][0].seq);
         break;
-
-    default:
-        assert.deepEqual(decoded, { a: data[i][0] });
+    case qbson.Long:
+        // NOTE: BSON.deserialize returns Long as a number if it fits!
+        assert.equal(decoded.a.high_, 0x10000000);
+        assert.equal(decoded.a.low_, 2);
         break;
+    default:
+        // bson.deserialize encodes `new Number(1)` as the empty object {}
     }
 }
 
