a67bd668bbc4a39fbe78b18bb27c18bd17766eac
Andras <andras@andrasq.com>
2019-03-12 15:00:04 -0400

    more decode tests

--- test-decode.js
+++ test-decode.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var assert = require('assert');
-var BSON = new (require('bson'))();
+var BSON = require('./bson');
 var qbson = require('./qbson');
 
 var data = [
@@ -25,6 +25,7 @@ var data = [
     function(x){ return x + 1234 },
 
     new qbson.MinKey(), new qbson.MaxKey(),
+    new Buffer("\x01\x00\x02"),
 
     // compound
     [], [1,2,3], [,,1,2,],
@@ -35,6 +36,55 @@ for (var i=0; i<data.length; i++) {
     if (isNaN(data[i])) assert(isNaN(qbson.decode(buf).a));
     else assert.deepStrictEqual(qbson.decode(buf).a, data[i]);
 }
+
+var buf = BSON.serialize({ a: new BSON.Long(1, -2) });
+var x = qbson.decode(buf);
+assert.equal(x.a.high32, -2 >>> 0);
+assert.equal(x.a.low32, 1);
+assert.equal(x.a.valueOf(), -2 * 0x100000000 + 1);
+
+var buf = BSON.serialize({ a: new BSON.Timestamp(1, -2) });
+var x = qbson.decode(buf);
+assert.equal(x.a.time, 1);
+assert.equal(x.a.seq, -2 >>> 0);
+
+var buf = BSON.serialize({ a: new BSON.MinKey() });
+var x = qbson.decode(buf);
+assert(x.a instanceof qbson.MinKey);
+
+var buf = BSON.serialize({ a: new BSON.MaxKey() });
+var x = qbson.decode(buf);
+assert(x.a instanceof qbson.MaxKey);
+
+var id = new qbson.ObjectId();
+var buf = qbson.encode({ a: new qbson.DbRef("refname", new qbson.ObjectId("00112233445566778899aabb")) });
+var x = qbson.decode(buf);
+assert.equal(x.a.$ref, 'refname');
+assert(x.a.$id instanceof qbson.ObjectId);
+assert.equal(x.a.$id.toString(), '00112233445566778899aabb');
+
+var obj = new qbson.ScopedFunction( function(abc){ return 123 + ab }, { ab: 12 });
+var buf = qbson.encode({ a: obj });
+var x = qbson.decode(buf);
+assert(x.a instanceof qbson.ScopedFunction);
+assert(typeof x.a.valueOf() === 'function');
+assert.deepEqual(x.a.valueOf()._scope, { ab: 12 });
+assert.equal(x.a.valueOf().toString(), x.a.func);
+assert.equal((x.a.valueOf())(1), 135);
+// FIXME:
+//assert(/^function\s*(abc)\s*{ return 123 + ab }$/.test(x.a.valueOf().toString()));
+
+// objectId
+var x = qbson.decode(new Buffer([20, 0, 0, 0, 7, 0x61, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 0]));
+assert(x.a instanceof qbson.ObjectId);
+assert.equal(String(x.a), '0102030405060708090a0b0c');
+
+// obsolete undefined type
+var x = qbson.decode(new Buffer([8, 0, 0, 0, 6, 0x61, 0, 0]));
+assert.deepStrictEqual(x, { a: undefined });
+
+// unknown type
+assert.throws(function() { qbson.decode(new Buffer([7, 0, 0, 0, 0x55, 0, 0])) }, /unsupported bson .* 0x55/);
 
 var buf, obj;
 
