3b83063b305799e529d9949da835a1218e7a954c
Andras <andras@andrasq.com>
2019-03-15 18:05:10 -0400

    fix test-make-function to tolerate the whitespace added or not added by different node versions

--- test/test-make-function.js
+++ test/test-make-function.js
@@ -3,15 +3,16 @@
 var assert = require('assert');
 var makeFunction = require('../make-function');
 
-var src = 'function() { return 1 }';
+var src = 'function tt() { return 1 }';
 assert.ok(typeof makeFunction(src), 'function');
-assert.equal(makeFunction(src).toString(), 'function () { return 1 }'); // note the inserted space before ()
+// older node insert a space between 'function' and '()', newer do not.  Name the function to avoid the issue.
+assert.equal(makeFunction(src).toString(), 'function tt() { return 1 }');
 assert.strictEqual(makeFunction(src)(), 1);
 
-var src = 'function() { return a + b }';
+var src = 'function tt() { return a + b }';
 var scope = { a: 1, b: '2' };
 assert.ok(typeof makeFunction(src, scope), 'function');
-assert.equal(makeFunction(src, scope).toString(), 'function () { return a + b }'); // note the inserted space before ()
+assert.equal(makeFunction(src, scope).toString(), 'function tt() { return a + b }');
 assert.strictEqual(makeFunction(src, scope)(), '12');
 
 assert.ok(makeFunction() instanceof Error);
